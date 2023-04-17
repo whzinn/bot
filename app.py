ddba5fadb120a3e72971a317a0e0d29ff1995f8e
nexo <65514701+whzinn@users.noreply.github.com>
2023-04-17 16:09:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ menu = """
 
 # Token do seu bot (você pode obtê-lo com o BotFather)
 TOKEN = '1995700005:AAFuK1Nk3aczl4SaHxMltrhc9jh0FYI0TYc'
+bot = telebot.TeleBot(TOKEN)
 bot.set_webhook(url="https://api.render.com/deploy/srv-cgu75vqut4mcfrh6b2h0?key=xLV_7EgG1wk")
 # Cria o objeto bot
-bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['menu'])
 def send_welcome(message):
     bot.reply_to(message, menu, parse_mode='Markdown')
